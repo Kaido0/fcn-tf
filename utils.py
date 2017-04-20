98a90f8049b3979a911d18a0ab91046b283e24fd
Kaido <328340975@qq.com>
2017-04-20 16:04:07 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.cm
+
+def color_image(image, num_classes=3):
+    norm = mpl.colors.Normalize(vmin=0., vmax=num_classes)
+    mycm = mpl.cm.get_cmap('Set1')
+    return mycm(norm(image))
